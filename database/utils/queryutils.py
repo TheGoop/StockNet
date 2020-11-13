113a4d7b35062066d82fba71595a31eb1197cd48
Neil Vaishampayan <neiluv@icloud.com>
2020-11-12 23:12:19 -0800

    Added more query utilities

--- database/utils/queryutils.py
+++ database/utils/queryutils.py
@@ -2,16 +2,61 @@ from database.payloadClasses.postcontententry import PostContentEntry
 
 
 def storePost(db,postID,postContent):
+    postID = str(postID)
     db.collection('Posts').document(postID).set(postContent.to_dict())
 
+
 def readPostbyID(db,postID):
+    postID = str(postID)
     doc_ref = db.collection('Posts').document(postID)
     doc = doc_ref.get()
     if doc.exists:
         return PostContentEntry.from_dict(doc.to_dict())
+    else:
+        raise KeyError("Post ID not found in database",postID)
+
+def storePostTag(db,tag,postID, taggedPostEntry):
+    postID = str(postID)
+    db.collection('Tags').collection(tag).document(postID).set(taggedPostEntry.to_dict())
+
+def storeAuthentication(db, username, authenticationEntry):
+    db.collection('Authentication').document(username).set(authenticationEntry.to_dict())
+
+def fetchDocumentsUnderTag(db,tag):
+    return db.collection('Tags').collection(tag).stream()
+
+
+def fetchPostsUnderTag(db,tag):
+    docs = fetchDocumentsUnderTag(db,tag)
+    if not docs:
+        raise KeyError("No posts found under tag",tag)
+    postContents = []
+    for doc in docs:
+        postContents.append(PostContentEntry.from_dict(doc.to_dict))
+    return postContents
+
+
+def __testSinglePostStore():
+    from database.utils.dbclientmanager import DBClientManager
+    manager = DBClientManager()
+    db = manager.getDBConnection()
+    post1 = PostContentEntry(5005,800,"Sample Post Test 1")
+    post2 = PostContentEntry(8000,900,"Sample Post Test 2",98,["+1"])
+    print(post1)
+    print(post2)
+    storePost(db,1,post1)
+    storePost(db, 2, post2)
+    post3 = readPostbyID(db,1)
+    post4 = readPostbyID(db,2)
+    print(post3)
+    print(post4)
+    assert (str(post1) == str(post3))
+    assert (str(post2) == str(post4))
+
 
 def runTests():
-    return
+    __testSinglePostStore()
+
 
 if __name__ == "__main__":
     runTests()
