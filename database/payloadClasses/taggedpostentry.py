b0b28e545b97a4c6eece58cd1297927189eda8f0
Neil Vaishampayan <neiluv@icloud.com>
2020-11-25 00:55:49 -0800

    Started adding transactional store operations, untested

--- database/payloadClasses/taggedpostentry.py
+++ database/payloadClasses/taggedpostentry.py
@@ -26,6 +26,9 @@ class TaggedPostEntry(object):
         )
 
 def runTests():
+    __testIdentity()
+
+def __testIdentity():
     entry1 = TaggedPostEntry(134957,"3AM")
     entry2 = TaggedPostEntry(1234521345,1645)
     print(entry1)
@@ -41,6 +44,17 @@ def runTests():
 
     assert (str(entry1) == str(entry3))
     assert (str(entry2) == str(entry4))
+
+##DEPRECATED##
+# def __testMinHeap():
+#     import datetime
+#     from datetime import timezone
+#     entry1 = TaggedPostEntry(1,datetime.datetime(2001,11,21,3,0,0,0,timezone.utc))
+#     entry2 = TaggedPostEntry(2, datetime.datetime(1997, 1, 2, 3, 0, 0, 0, timezone.utc))
+#     entry3 = TaggedPostEntry(3, datetime.datetime(2003, 11, 21, 3, 0, 0, 0, timezone.utc))
+#     entry4 = TaggedPostEntry(4, datetime.datetime(2003, 10, 21, 3, 0, 0, 0, timezone.utc))
+#     entry5 = TaggedPostEntry(5, datetime.datetime(2003, 11, 20, 3, 0, 0, 0, timezone.utc))
+
 
 if __name__ == "__main__":
     runTests()
--- database/utils/queryutils.py
+++ database/utils/queryutils.py
@@ -1,15 +1,46 @@
 from firebase_admin import firestore
 
 from database.payloadClasses.postcontententry import PostContentEntry
+from database.payloadClasses.taggedpostentry import TaggedPostEntry
 
 
 def storePost(db,postID,postContent):
     postID = str(postID)
     db.collection('Posts').document(postID).set(postContent.to_dict())
 
-def storePostTag(db,tag,postID, taggedPostEntry):
-    postID = str(postID)
-    db.collection('Tags').collection(tag).document(postID).set(taggedPostEntry.to_dict())
+def storePostTag(db,tag, taggedPostEntry):
+    #document 'tag' format
+    transaction = db.transaction()
+    postRef = db.collection("Tags").document(tag)
+    result = __transactionalPostTagStore(transaction,postRef)
+    return result
+
+@firestore.transactional
+def __transactionalPostTagStore(transaction, postRef, taggedPostEntry):
+    snapshot = postRef.get(transaction=transaction)
+    prevPosts = []
+    if snapshot.exists:
+        prevPosts = snapshot.to_dict()['posts']
+        currTime = taggedPostEntry.time
+        inserted = False
+        for (i, prevPostEntries) in enumerate(prevPosts):
+            if currTime >= prevPostEntries["time"]:
+                prevPosts.insert(i,taggedPostEntry.to_dict())
+                inserted = True
+                break
+        if not inserted:
+            prevPosts.append(taggedPostEntry.to_dict())
+        transaction.update(postRef,{
+            'posts': prevPosts
+        })
+        return True
+    else:
+        prevPosts.append(taggedPostEntry.to_dict())
+        transaction.set(postRef,prevPosts)
+        return True
+    return False
+
+
 
 def storeAuthentication(db, username, authenticationEntry):
     db.collection('Authentication').document(username).set(authenticationEntry.to_dict())
@@ -35,6 +66,7 @@ def readPostbyID(db,postID):
     else:
         raise KeyError("Post ID not found in database",postID)
 
+#TODO change tag subcollection to a document containing a list of taggedPostEntry
 def fetchDocumentsUnderTag(db,tag):
     return db.collection('Tags').collection(tag).stream()
 
@@ -49,9 +81,7 @@ def fetchPostsUnderTag(db,tag):
     return postContents
 
 
-def __testSinglePostStore():
-    from database.utils.dbclientmanager import DBClientManager
-    manager = DBClientManager()
+def __testSinglePostStore(manager):
     db = manager.getDBConnection()
     post1 = PostContentEntry(5005,800,"Sample Post Test 1")
     post2 = PostContentEntry(8000,900,"Sample Post Test 2",98,["+1"])
@@ -66,9 +96,44 @@ def __testSinglePostStore():
     assert (str(post1) == str(post3))
     assert (str(post2) == str(post4))
 
+def __testTagging(manager):
+    db = manager.getDBConnection()
+    postID1 = 1001
+    postID2 = 1002
+    postID3 = 1003
+    postID4 = 1004
+    post1 = PostContentEntry(1, 800, "Tagged Post 1")
+    post2 = PostContentEntry(2, 800, "Tagged Post 2")
+    post3 = PostContentEntry(3, 800, "Tagged Post 3")
+    post4 = PostContentEntry(4, 800, "Tagged Post 4")
+    tagEntry = TaggedPostEntry(800)
+    storePostTag(db,"Tag1",postID1,tagEntry)
+    storePostTag(db,"Tag2",postID2,tagEntry)
+    storePostTag(db,"Tag2",postID3,tagEntry)
+    storePostTag(db,"Tag2",postID4,tagEntry)
+    storePost(db,postID1,post1)
+    storePost(db,postID2,post2)
+    storePost(db,postID3,post3)
+    storePost(db,postID4,post4)
+    print(post1)
+    print(post2)
+    print(post3)
+    print(post4)
+    tag1Posts = fetchPostsUnderTag(db,"Tag1")
+    tag2Posts = fetchPostsUnderTag(db,"Tag2")
+    for post in tag1Posts:
+        print(post)
+    for post in tag2Posts:
+        print(post)
+
+
+
 
 def runTests():
-    __testSinglePostStore()
+    from database.utils.dbclientmanager import DBClientManager
+    manager = DBClientManager()
+    __testSinglePostStore(manager)
+    __testTagging(manager)
 
 
 if __name__ == "__main__":
