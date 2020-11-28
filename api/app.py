7d7cff51513d5c412d36a8c007767f0571f42055
Akshay Gupta <akshaygupta.us@gmail.com>
2020-11-28 02:48:57 -0500

    Have template GETS working for flask - confirmation that DJANGO too heavy for this because we using Firestore

--- api/app.py
+++ api/app.py
@@ -1,18 +1,26 @@
 from flask import Flask
-
+from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route('/endpoint/singlepost?<id>', methods = ['GET'])
-def get_post(id):
-    #id is the post id
-    return (id)
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/endpoint/singlepost/<int:id>', methods=['GET'])
+def get_tasks(id):
+    return str(id)
 
 @app.route('/endpoint/postpreview?stock=<name>&num=<int:X>', methods = ['GET'])
 def get_user(name, X = 0):
-    return (name, x)
+    return str((name, str(x)))
 
-
-
+@app.route('/createcm', methods=['get'])
+def create_cm():
+    summary = request.args.get('summary', None) # use default value repalce 'None'
+    change = request.args.get('change', None)
+    # do something, eg. return json response
+    return jsonify({'summary': summary, 'change': change})
 
 if __name__ == '__main__':
     app.run(debug=True)
