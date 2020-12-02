cf8020fb396ca58b58b03429142a0f693b3995b2
Akshay Gupta <akshaygupta.us@gmail.com>
2020-12-02 01:47:44 -0500

    Forgot to add userAuthAPI.py to previous commit, here it is. Tested and works with functionality as specified in previous commits and comments in code.

---
+++ apis/backendApi/userAuthAPI.py
@@ -0,0 +1,28 @@
+from database.payloadClasses.authenticationEntry import AuthenticationEntry
+from database.utils import queryutils
+
+def is_valid_login(db, body):
+    if 'username' in body and 'password' in body:
+        username = body['username']
+        password = body['password']
+    
+    else:
+        # if either username or password not given in body
+        return 3
+
+    try:
+        authEntry = queryutils.fetchAuthentication(db, username)
+    #if the username is not found, will throw KeyError
+    except KeyError:
+        return 2
+    #Undefined Behavior
+    except Exception:
+        return 4
+
+    #Return 0 to authenticate user,
+    if (authEntry.password == password):
+        return 0
+    #return 1 if user given bad password
+    else:
+        return 1
+
